c7d067441faa0db085502512b6c322ed08ed12cc
Torres456 <barrientostorres9@gmail.com>
2024-10-24 14:18:49 -0600

    agregue ruta

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
