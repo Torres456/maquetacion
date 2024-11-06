9da9231ca9134c01ef0d9743f3d3b80251d62ece
MarioGV1234 <m.orlando0309@gmail.com>
2024-11-06 13:01:23 -0600

    Agregamos ruta para barra lateral y panel

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('panel-cliente');
-    
 });
 
 Route::get('/carrusel', [CarruselController::class, 'showCarrusel']);
