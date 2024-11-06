ce09d19c7de30d55999318be870382022b8b602b
MarioGV1234 <m.orlando0309@gmail.com>
2024-11-06 13:06:13 -0600

    Correccion de error

--- resources/views/panel-cliente.blade.php
+++ resources/views/panel-cliente.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-        <x-header />
+        
             <body class="dark:bg-gray-600">
                             
             <button data-drawer-target="sidebar" data-drawer-toggle="sidebar" aria-controls="sidebar" type="button" class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
@@ -29,7 +29,7 @@
                                     <span class="ms-3">Comienzo</span>
                                 </a>
                             </li> 
-                                              
+
                             <li>
                                 <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                 <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
